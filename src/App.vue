d2af657a9b95d155539be56704f1ef489e0c7924
Ferdinand Lanvino <frdnndvn106@gmail.com>
2025-05-16 18:15:55 +0700

    Refactor App.vue and add styling for the schedule generator form

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Test 1</h1>
+
+    <div class="form">
+      <div class="form-group">
+        <label for="shift">Pilih Shift</label>
+        <select id="shift" v-model="shift" @change="generateSchedule">
+          <option value="A">Shift A</option>
+          <option value="B">Shift B</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label for="workingHour">Jam Kerja</label>
+        <input type="number" id="workingHour" v-model="workingHour" @change="generateSchedule" />
+      </div>
+
+      <button @click="generateSchedule">Tampilkan Jadwal Kerja</button>
+    </div>
+
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      shift: 'A', // Default shift
+      workingHour: 8, // Default working hour
+    };
+  },
+  methods: {
+    generateSchedule() {
+      // Implement your schedule generation logic here
+      //Add validation for working hour
+      if (this.workingHour > 10) {
+        alert('Maksimal jam kerja adalah 10 jam');
+        return;
+      }
+
+      if (this.workingHour < 1) {
+        alert('Minimal jam kerja adalah 1 jam');
+        return;
+      }
+      
+      console.log('Selected Shift:', this.shift);
+      console.log('Working Hour:', this.workingHour);
+    }
   }
 }
 </script>
 
+<!-- Import styling from css -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style.css';
 </style>
